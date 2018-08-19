e35dbd7aa6c98e84ba3e562763c9d1f888f53237
ehCruz <cruzeduardohenrique@gmail.com>
2018-08-19 18:12:02 -0300

    Ajustes finais atividade  Teatro

--- teatro/Teatro.java
+++ teatro/Teatro.java
@@ -21,7 +21,8 @@ public class Teatro {
 		System.out.print("\033\143");
 		int op;
 		while (true) {
-			System.out.println("Selecione uma opcao:\n1- Reservar assento\n2- Listar assentos\n9- Sair");
+			System.out.print(
+					"Controle de ocupacao de teatro\n1- Reservar assento\n2- Listar assentos\n9- Sair\nSelecione uma opcao:");
 			op = sc.nextInt();
 			switch (op) {
 			case 1:
@@ -49,28 +50,25 @@ public class Teatro {
 	private void reservarAssento() {
 		int opcao = 0;
 		do {
-			System.out.printf("%sEscolha o tipo do assento:%n1- Plateia %n2- Balcao%n", "\033\143");
+			System.out.printf("%sTipo do assento:%n1- Plateia %n2- Balcao%nSelecione uma opcao:", "\033\143");
 			opcao = sc.nextInt();
 		} while (opcao <= 0 && opcao > 2);
 		switch (opcao) {
 		case 1:
-			if (!verificarOcupacao()) {
+			if (!verificarOcupacao(0)) {
+				System.out.printf("%s--Reservar assento na plateia:%n", "\033\143");
 				int fila;
 				int cadeira;
-				boolean naoValido = true;
 				do {
-					System.out.println("Informe a fila(0 - 9):");
+					System.out.printf("Informe a fila(0 - %d):", assentos.length - 1);
 					fila = sc.nextInt();
-					if (fila >= 0 && fila <= 9)
-						naoValido = false;
-				} while (naoValido);
-				naoValido = true;
+				} while (!(fila >= 0 && fila < assentos.length));
+
 				do {
-					System.out.println("Informe a cadeira(0 - 9):");
+					System.out.printf("Informe a cadeira(0 - %d):", assentos[fila].length - 1);
 					cadeira = sc.nextInt();
-					if (cadeira >= 0 && cadeira <= 9)
-						naoValido = false;
-				} while (naoValido);
+				} while (!(cadeira >= 0 && cadeira < assentos[fila].length));
+
 				if (assentos[fila][cadeira] == 0) {
 					assentos[fila][cadeira] = 1;
 					System.out.print("\033\143");
@@ -86,7 +84,38 @@ public class Teatro {
 			}
 			break;
 		case 2:
-			System.out.println("Reservar balcao");
+			if (!(verificarOcupacao(1))) {
+				System.out.printf("%s--Reservar assento no balcao:%n", "\033\143");
+				int posicaoBalcao;
+				int fila;
+				int cadeira;
+				do {
+					System.out.printf("Informe a posicao do balcao(0 - %d):", balcoes.length - 1);
+					posicaoBalcao = sc.nextInt();
+				} while (!(posicaoBalcao >= 0 && posicaoBalcao < balcoes.length));
+
+				do {
+					System.out.printf("Informe a fila(0 - %d):", balcoes[posicaoBalcao].length - 1);
+					fila = sc.nextInt();
+				} while (!(fila >= 0 && fila < balcoes[posicaoBalcao].length));
+
+				do {
+					System.out.printf("Informe a cadeira(0 - %d):", balcoes[posicaoBalcao][fila].length - 1);
+					cadeira = sc.nextInt();
+				} while (!(cadeira >= 0 && cadeira < balcoes[posicaoBalcao][fila].length));
+
+				if (balcoes[posicaoBalcao][fila][cadeira] == 0) {
+					balcoes[posicaoBalcao][fila][cadeira] = 1;
+					System.out.print("\033\143");
+					System.out.printf("Balcao P%d: cadeira N%d da fila N%d foi reservado com sucesso%n", posicaoBalcao,
+							cadeira, fila);
+				} else {
+					System.out.print("\033\143");
+					System.out.printf(
+							"Balcao P%d: cadeira N%d da fila N%d ja esta reservado, tente novamente outro lugar%n",
+							posicaoBalcao, cadeira, fila);
+				}
+			}
 			break;
 		}
 	}
@@ -95,7 +124,7 @@ public class Teatro {
 		int opcao = 0;
 		do {
 			System.out.printf(
-					"%sListar Assentos%nSelecione o tipo de asseto que deseja listar%n1- Plateia%n2- Balcao%n3- Plateia e Balcao%n",
+					"%sListar Assentos%nTipo de assento que deseja listar%n1- Plateia%n2- Balcao%n3- Plateia e Balcao%nSelecione uma opcao:",
 					"\033\143");
 			opcao = sc.nextInt();
 		} while (opcao <= 0 && opcao > 3);
@@ -132,30 +161,34 @@ public class Teatro {
 			System.out.print("\n\n");
 			break;
 		case 3:
-			System.out.print("--Plateia\n");
+			System.out.print("\033\143");
+			System.out.print("--Plateia\n\n");
 			for (int fila = 0; fila < assentos.length; fila++) {
-				System.out.printf("Fila %d%n        ", fila + 1);
+				System.out.print("   ");
 				for (int cadeira = 0; cadeira < assentos[fila].length; cadeira++) {
 					if (assentos[fila][cadeira] == 0)
 						System.out.print("Vazio ");
 					else
 						System.out.print("Reservado ");
 				}
-				System.out.print("\n\n");
+				System.out.print("\n");
 			}
-			System.out.print("\n\n--Balcao\n");
+			System.out.print("\n\n");
+			System.out.print("--Balcao\n\n");
 			for (int total = 0; total < balcoes.length; total++) {
-				System.out.printf("Balcao %d%n        ", total + 1);
 				for (int fila = 0; fila < balcoes[total].length; fila++) {
+					System.out.print("   ");
 					for (int cadeira = 0; cadeira < balcoes[total][fila].length; cadeira++) {
 						if (balcoes[total][fila][cadeira] == 0)
 							System.out.print("Vazio ");
 						else
 							System.out.print("Reservado ");
 					}
-					System.out.print("\n\n");
+					System.out.print("\n");
 				}
+				System.out.print("\n");
 			}
+			System.out.print("\n\n");
 			break;
 		}
 	}
@@ -169,13 +202,25 @@ public class Teatro {
 		listarAssentos();
 	}
 
-	private boolean verificarOcupacao() {
-		for (int fila = 0; fila < assentos.length; fila++) {
-			for (int cadeira = 0; cadeira < assentos[fila].length; cadeira++) {
-				if (assentos[fila][cadeira] == 0)
-					return false;
+	private boolean verificarOcupacao(int tipoAssento) {
+		if (tipoAssento == 0) {
+			for (int fila = 0; fila < assentos.length; fila++) {
+				for (int cadeira = 0; cadeira < assentos[fila].length; cadeira++) {
+					if (assentos[fila][cadeira] == 0)
+						return false;
+				}
 			}
+			return true;
+		} else {
+			for (int total = 0; total < balcoes.length; total++) {
+				for (int fila = 0; fila < balcoes[total].length; fila++) {
+					for (int cadeira = 0; cadeira < balcoes[total][fila].length; cadeira++) {
+						if (balcoes[total][fila][cadeira] == 0)
+							return false;
+					}
+				}
+			}
+			return true;
 		}
-		return true;
 	}
 }
